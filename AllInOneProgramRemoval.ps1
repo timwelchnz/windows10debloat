348850e8e50f7ba3489babc1908f15d819065c07
timwelchnz <tim.welch.nz@gmail.com>
2021-03-30 12:27:16 +1300

    Update AllInOneProgramRemoval.ps1

--- AllInOneProgramRemoval.ps1
+++ AllInOneProgramRemoval.ps1
@@ -2,14 +2,62 @@
 # Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
 Add-Type -AssemblyName System.Windows.Forms
 Clear-Host
+#We're immediately going to remove all of the below... and afterwards loop through the remaining...
 $DefaultRemove = @(
-
+    "AD2F1837.HPPrivacySettings"
+    "AD2F1837.HPProgrammableKey"
+    "AD2F1837.HPQuickDrop"
+    "AD2F1837.HPSureShieldAI"
+    "AD2F1837.myHP"
+    "AppUp.IntelGraphicsExperience"
+    "AppUp.IntelManagementandSecurityStatus"
+    "AppUp.ThunderboltControlCenter"
+    "Microsoft.549981C3F5F10"
+    "Microsoft.BingWeather"
+    "Microsoft.GetHelp"
+    "Microsoft.Getstarted"
+    "Microsoft.Microsoft3DViewer"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"
+    "Microsoft.MicrosoftStickyNotes"
+    "Microsoft.MixedReality.Portal"
+    "Microsoft.Office.OneNote"
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    "Microsoft.Wallet"
+    "Microsoft.Whiteboard"
+    "Microsoft.WindowsAlarms"
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.WindowsFeedbackHub"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsSoundRecorder"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.XboxApp"
+    "Microsoft.XboxGameOverlay"
+    "Microsoft.XboxGamingOverlay"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.YourPhone"
+    "Microsoft.ZuneMusic"
+    "Microsoft.ZuneVideo"
+    "NVIDIACorp.NVIDIAControlPanel"
 )
 
-ForEach ($remove in $DefaultRemove) {
-  
+ForEach ($toremove in $DefaultRemove) {
+    Get-ProvisionedAppxPackage -Online | Where-Object DisplayName -EQ $toremove.DisplayName | Remove-ProvisionedAppxPackage -Online -AllUsers
 }
 
+$continue = [System.Windows.Forms.MessageBox]::Show("Do you want to continue through remaining?","Batch Windows 10 App Removal", "YesNo" , "Information" , "Button1")
+Switch ($continue) {
+    'Yes' {}
+    'No' {
+      Exit
+    }
+
+}
+
+
+# Now retrieve remaining Provisioned Packages...
 $ProvisionedFiles = @(Get-ProvisionedAppxPackage -Online | Select-Object DisplayName)
 
 ForEach ($files in $ProvisionedFiles) {
