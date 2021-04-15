7479336de91646db8f868a4057936e783d7f1eb1
timwelchnz <tim.welch.nz@gmail.com>
2021-04-16 10:52:19 +1200

    Update

--- AllInOneProgramRemoval.ps1
+++ AllInOneProgramRemoval.ps1
@@ -15,8 +15,11 @@ $registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
 $Name = "SearchboxTaskbarMode"
 $value = "1"
 
-If((Get-ItemPropertyValue -path $registryPath -Name $Name) -ne $value) {
-    Set-ItemProperty -Path $registryPath -Name $Name -Value $value
+$SearchBar = Get-Item -Path $registryPath
+If($SearchBar.GetValue($Name) -eq $null) {
+  New-ItemProperty -Path $registryPath -Name $Name -Value $value -PropertyType DWord
+} else {
+  Set-ItemProperty -Path $registryPath -Name $Name -Value $value
 }
 
 #Remove Task View Button
