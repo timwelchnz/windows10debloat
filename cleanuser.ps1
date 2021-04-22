f5bf2fa404e79d64d2ece7542f473e20bd9a904f
Tim Welch <tim.welch.nz@gmail.com>
2021-04-23 07:56:09 +1200

    Commit

---
+++ cleanuser.ps1
@@ -0,0 +1,49 @@
+Function Log {
+  param(
+      [Parameter(Mandatory=$true)][String]$msg
+  )
+  Add-Content -Path $env:TEMP\log.txt $msg
+}
+
+Log "Unpin Microsoft Store"
+#Unpin Microsoft Store from Taskbar - https://docs.microsoft.com/en-us/answers/questions/214599/unpin-icons-from-taskbar-in-windows-10-20h2.html
+$appname = "Microsoft Store"
+((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | %{$_.DoIt(); $exec = $true}
+
+Log "Set Search Bar to Icon"
+#Set Search Bar to Icon
+$registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search"
+$Name = "SearchboxTaskbarMode"
+$value = "1"
+$SearchBar = Get-Item -Path $registryPath
+If($null -eq $SearchBar.GetValue($Name)) {
+  New-ItemProperty -Path $registryPath -Name $Name -Value $value -PropertyType DWord
+} else {
+  Set-ItemProperty -Path $registryPath -Name $Name -Value $value
+}
+
+Log "Remove Task View Button"
+#Remove Task View Button
+$registryPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$Name = "ShowTaskViewButton"
+$value = "0"
+$TaskBar = Get-Item -Path $registryPath
+If($null -eq $TaskBar.GetValue($Name)) {
+  New-ItemProperty -Path $registryPath -Name $Name -Value $value -PropertyType DWord
+} else {
+  Set-ItemProperty -Path $registryPath -Name $Name -Value $value
+}
+
+Log "Remove Meet Now from Taskbar"
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$Name = "HideSCAMeetNow"
+$value = "1"
+$Exist = "Get-ItemProperty -Path $registryPath -Name $Name"
+if ($Exist)
+{
+    Set-ItemProperty -Path $Path -Name $Name -Value $value
+}
+Else
+{
+    New-ItemProperty -Path $Path -Name $Name -Value $value -PropertyType DWord
+}
