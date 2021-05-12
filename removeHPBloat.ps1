71c537b045700aaa371a5eb33ba43aa7527ba899
Tim Welch <tim.welch.nz@gmail.com>
2021-05-12 17:59:36 +1200

    HP Debloat

--- removeHPBloat.ps1
+++ removeHPBloat.ps1
@@ -30,3 +30,12 @@ foreach( $App in $Apps ){
 
     Start-Process -FilePath cmd -ArgumentList '/c', $UninstallString -NoNewWindow -Wait
     }
+
+# Remove HP Shortcuts
+Remove-Item -LiteralPath "C:\ProgramData\HP\TCO" -Force -Recurse
+Remove-Item -LiteralPath "C:\Online Services" -Force -Recurse
+Remove-Item -Path "C:\Users\Public\Desktop\TCO Certified.lnk" -Force -Recurse
+
+#Remove Adobe Trial Shortcuts
+Remove-Item -LiteralPath "C:\Program Files (x86)\Online Services" -Force -Recurse
+Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proefversies.lnk" -Force -Recurse
