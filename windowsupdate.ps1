8a0f891df838c385385bc211192d0a3758079996
Tim Welch <tim.welch.nz@gmail.com>
2021-06-11 08:26:35 +1200

    Windows Update

--- windowsupdate.ps1
+++ windowsupdate.ps1
@@ -1,3 +1,13 @@
+#Run Windows Updates
+
+$PackageProvider = "NuGet"
+if (Get-PackageProvider -ListAvailable -Name $PackageProvider) {
+    Write-Host "NuGet Installed"
+}
+else {
+    Install-PackageProvider -Name NuGet -Force
+}
+
 $Module = "PSWindowsUpdate"
 if (Get-Module -ListAvailable -Name $Module) {
     Write-Host "Running Windows Update"
@@ -5,5 +15,5 @@ if (Get-Module -ListAvailable -Name $Module) {
 else {
     Install-Module PSWindowsUpdate -Confirm:$false -Force
 }
-Get-WindowsUpdate -Confirm:$false
-Install-WindowsUpdate -Confirm:$false
+Get-WindowsUpdate -install -acceptall -Confirm:$false
+# Install-WindowsUpdate -Confirm:$false
