23c7fb7e2afbf6f1e8eeab8649b95a805eeec9be
timwelchnz <tim.welch.nz@gmail.com>
2021-06-17 12:24:40 +1200

    Test Connection

--- startclean-wifi.ps1
+++ startclean-wifi.ps1
@@ -37,10 +37,26 @@ $wlanProfile = @'
 '@
 Set-Content -Path $env:TEMP\wlan.xml -Value $wlanProfile
 netsh wlan add profile filename=$env:TEMP\wlan.xml
-if (-Not (Test-Connection raw.githubusercontent.com -Quiet)) {
-    Write-Host "Unable to connect to the internet"
+
+# Test Network Connection
+[int]$SleepTimer = "2" #seconds to attempt after
+[int]$Attempts = "5"
+$AttemptsCounter = 0
+$RemainingAttempts = $Attempts - $AttemptsCounter
+Write-Host "Testing to see if netowrk connection is avilable..."
+while($RemainingAttempts -gt 0) {
+    if(Test-Connection raw.githubusercontent.com -Quiet -Count 1) {
+        Write-Host "Network connection is Good!" -BackgroundColor Green -ForegroundColor Black
+        break
+    } else {
+        Write-Host "Network is not connected. Retrying..." -BackgroundColor Red
+        Start-Sleep -Seconds ($SleepTimer)
+        $RemainingAttempts--
+    }
+}
+if($RemainingAttempts -eq 0) {
+    Write-Host "Maximum number of attempts reached trying to connection to the internet"
+    Write-Host "Internet Connection Failed" -BackgroundColor Red
     Exit
-} Else {
-    Write-Host "Connection Good"
 }
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/timwelchnz/windows10debloat/main/AllInOneProgramRemoval.ps1'))
