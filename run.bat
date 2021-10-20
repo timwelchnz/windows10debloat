cf38811f37cae5cea351e91dce81825d499e4d7d
timwelchnz <tim.welch.nz@gmail.com>
2021-10-20 15:36:27 +1300

    Update

--- run.bat
+++ run.bat
@@ -2,7 +2,8 @@
 @goto :EOF
 
 $WiFi = netsh wlan show networks
-If ($Wifi -match 'Wcomp Dirty') {
+$networks = (($Wifi -match '\d\s:').split(":") -replace "^SSID\s\d+").trim() | ? {$_.trim() -ne ""
+If ($networks -match 'Wcomp Dirty') {
 	$wlanProfile = @'
 	<?xml version="1.0"?>
 	<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
