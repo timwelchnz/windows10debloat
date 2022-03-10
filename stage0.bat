1730e4a3abd3f9c1de03b5707c6bff6851ddf199
timwelchnz <tim.welch.nz@gmail.com>
2022-03-11 12:38:41 +1300

    Clean Up

--- stage0.bat
+++ stage0.bat
@@ -1,48 +1,8 @@
 @powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$_=((Get-Content \"%~f0\") -join \"`n\");iex $_.Substring($_.IndexOf(\"goto :\"+\"EOF\")+9)"
 @goto :EOF
 If (-Not (Get-NetConnectionProfile).IPv4Connectivity -contains "Internet") { 
-	$Wifi = netsh wlan show networks
-	$networks = (($Wifi -match '\d\s:').split(":") -replace "^SSID\s\d+").trim() | ? {$_.trim() -ne ""}
-	If ($networks -match "Wcomp Dirty") {
-	$wlanProfile = @'
-		<?xml version="1.0"?>
-		<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-			<name>Wcomp Dirty</name>
-			<SSIDConfig>
-				<SSID>
-					<hex>57636F6D70204469727479</hex>
-					<name>Wcomp Dirty</name>
-				</SSID>
-			</SSIDConfig>
-			<connectionType>ESS</connectionType>
-			<connectionMode>manual</connectionMode>
-			<MSM>
-				<security>
-					<authEncryption>
-						<authentication>WPA2PSK</authentication>
-						<encryption>AES</encryption>
-						<useOneX>false</useOneX>
-					</authEncryption>
-					<sharedKey>
-						<keyType>passPhrase</keyType>
-						<protected>false</protected>
-						<keyMaterial>1qazZAQ!</keyMaterial>
-					</sharedKey>
-				</security>
-			</MSM>
-			<MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3">
-				<enableRandomization>false</enableRandomization>
-				<randomizationSeed>3678992080</randomizationSeed>
-			</MacRandomization>
-		</WLANProfile>
-'@
-	Set-Content -Path $env:TEMP\wlan.xml -Value $wlanProfile
-	netsh wlan add profile filename=$env:TEMP\wlan.xml
-	}
-	Else {
-		Write-host "Manually connect Wi-Fi or Ethernet..." -BackgroundColor Red -ForegroundColor Black
-		Read-host -Prompt "Press a key to continue when connected"
-	}
+	Write-host "Manually connect Wi-Fi or Ethernet..." -BackgroundColor Red -ForegroundColor Black
+	Read-host -Prompt "Press a key to continue when connected"
 }
 # Test Network Connection
 [int]$SleepTimer = "2" #seconds to attempt after 
