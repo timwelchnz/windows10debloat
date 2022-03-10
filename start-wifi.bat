19ac36df2b826dd259776743c6d4811a9962b1f8
timwelchnz <tim.welch.nz@gmail.com>
2022-03-11 12:41:20 +1300

    Clean Up

--- start-wifi.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-PowerShell.exe -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""startclean-wifi.ps1""' -Verb RunAs}"
-PowerShell.exe -NoProfile -Command "Write-Host $env:TEMP\log.txt"
